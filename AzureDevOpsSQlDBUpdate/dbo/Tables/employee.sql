d310ce559d2b886a9ea1cd9426cb37e28a98cb0a
Riyaz Ahmed <riyaz.ahmed_sk@mercedes-benz.com>
2024-05-13 04:07:31 +0000

    updated

---
+++ AzureDevOpsSQlDBUpdate/dbo/Tables/employee.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[employee] (
+    [EmpId]     INT             NOT NULL,
+    [FirstName] VARCHAR (50)    NULL,
+    [LastName]  VARCHAR (50)    NULL,
+    [Email]     VARCHAR (70)    NULL,
+    [DOB]       DATE            NULL,
+    [Salary]    DECIMAL (10, 2) NULL,
+    PRIMARY KEY CLUSTERED ([EmpId] ASC)
+);
+
